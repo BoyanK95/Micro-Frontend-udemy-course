4cd2923bc5884ce5a5d029efabab43b9e12e4462
BoyanK95 <boyan_k_95@abv.bg>
2023-05-18 00:57:18 +0300

    fix(container): fix container widget to consume products widget

--- container/src/bootstrap.js
+++ container/src/bootstrap.js
@@ -1,4 +1,7 @@
-import 'products/ProductsIndex';
-import 'cart/CartShow'
+import { mount } from 'products/ProductsIndex';
+import 'cart/CartShow';
 
 console.log('Container!');
+
+const root = document.getElementById('my-products');
+mount(root);
