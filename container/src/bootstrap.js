93b41fdc058c9e5bab802c987a44df29e18bbee3
BoyanK95 <boyan_k_95@abv.bg>
2023-05-18 09:12:31 +0300

    refactore(cart): refactore cart to moutn async regardless of root container

--- container/src/bootstrap.js
+++ container/src/bootstrap.js
@@ -1,7 +1,9 @@
-import { mount } from 'products/ProductsIndex';
-import 'cart/CartShow';
+import { mount as mountProducts } from 'products/ProductsIndex';
+import { mount as mountCart } from 'cart/CartShow';
 
 console.log('Container!');
 
-const root = document.getElementById('my-products');
-mount(root);
+const productsRoot = document.getElementById('my-products');
+const cartRoot = document.getElementById('my-cart');
+mountProducts(productsRoot);
+mountCart(cartRoot);
