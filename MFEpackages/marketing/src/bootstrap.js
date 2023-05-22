ede2fa74a9b7bca8994d9f497c5c2f99af5e62a5
BoyanK95 <boyan_k_95@abv.bg>
2023-05-22 20:05:10 +0300

    fix: fix routing from container to child

--- MFEpackages/marketing/src/bootstrap.js
+++ MFEpackages/marketing/src/bootstrap.js
@@ -6,16 +6,28 @@ import App from './App';
 const mount = (el, { onNavigate }) => {
     const history = createMemoryHistory();
 
-    history.listen(onNavigate);
+    if (onNavigate) {
+        history.listen(onNavigate);
+    }
 
     ReactDom.render(<App history={history} />, el);
+
+    return {
+        onParentNavigate({ pathname: nextPathname }) {
+            const { pathname } = history.location;
+
+            if (pathname !== nextPathname) {
+                history.push(nextPathname);
+            }
+        }
+    };
 };
 
 if (process.env.NODE_ENV === 'development') {
     const devRoot = document.getElementById('_marketing-dev-root');
 
     if (devRoot) {
-        mount(devRoot);
+        mount(devRoot, {});
     }
 }
 
