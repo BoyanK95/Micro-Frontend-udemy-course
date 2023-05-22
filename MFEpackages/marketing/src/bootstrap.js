a8ae2c35b0d98868017df2e1feb585860b527b02
BoyanK95 <boyan_k_95@abv.bg>
2023-05-22 18:32:32 +0300

    feat: create browser navigation in MarketingApp

--- MFEpackages/marketing/src/bootstrap.js
+++ MFEpackages/marketing/src/bootstrap.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import { createMemoryHistory } from 'history';
 import App from './App';
 
-const mount = (el) => {
-    ReactDom.render(<App />, el);
+const mount = (el, { onNavigate }) => {
+    const history = createMemoryHistory();
+
+    history.listen(onNavigate);
+
+    ReactDom.render(<App history={history} />, el);
 };
 
 if (process.env.NODE_ENV === 'development') {
